693c7a2d6efd8797a0f0a301c50ac31105e1fc11
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-27 16:38:24 +0000

    set up cat and advice api's and routes

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/meowtivation") do
+  raw_data = HTTP.get("https://api.adviceslip.com/advice")
+  parsed_data = JSON.parse(raw_data).fetch("slip")
+  @advice = parsed_data.fetch("advice")
+
+  
+  erb(:cat_advice)
+end
+
+get("/meowdy") do
+  cat_data = HTTP.get("https://api.thecatapi.com/v1/images/search")
+  parsed_cat_data = JSON.parse(cat_data).first
+  @cat_image = parsed_cat_data.fetch("url")
+  
+  erb(:cat_image)
 end
